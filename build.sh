30055a4044d4eaccf47572aafbb5b40d23edc1a4
Kalinin Nikolay <ktulhuftf@gmail.com>
2019-03-04 17:59:53 +0500

    update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ YELLOW='\033[0;33m'     #  ${YELLOW}
 # Exit if any of the intermediate steps fail
 set -e
 echo "${YELLOW}==> build.sh: going to windows templates directory ${YELLOW}\n"
-# cd windows
+cd windows
 echo "${YELLOW}==> build.sh: building windows server 2019 box from template ${YELLOW}\n"
-# packer build -only=virtualbox-iso 2019.json
+packer build -only=virtualbox-iso 2019.json
 echo "${YELLOW}==> build.sh: importing windows server 2019 box to vagrant ${YELLOW}\n"
-# vagrant box add  win2019-std windows-2019-standard-virtualbox.box
+vagrant box add  win2019-std windows-2019-standard-virtualbox.box
 echo "${YELLOW}==> build.sh: list aviliable vagrant boxes ${YELLOW}\n"
 vagrant box list 
