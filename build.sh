8a952f0767adce39216a4ab2408b7f71a49732bc
Kalinin Nikolay <ktulhuftf@gmail.com>
2019-03-04 17:51:10 +0500

    butify build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-
+#set text color
+YELLOW='\033[0;33m'     #  ${YELLOW}
 # Exit if any of the intermediate steps fail
 set -e
-
-# go 
-cd windows
-
-# building windows server 2019 box
-packer build -only=virtualbox-iso 2019.json
-
-# add windows server 2019 box to vagrant
-vagrant box add  win2019-std windows-2019-standard-virtualbox.box
-
-# list aviliable vagrant boxes
-vagrant box list | grep 'win2019-std'
+echo "${YELLOW}==> build.sh: going to windows templates directory ${YELLOW}\n"
+# cd windows
+echo "${YELLOW}==> build.sh: building windows server 2019 box from template ${YELLOW}\n"
+# packer build -only=virtualbox-iso 2019.json
+echo "${YELLOW}==> build.sh: importing windows server 2019 box to vagrant ${YELLOW}\n"
+# vagrant box add  win2019-std windows-2019-standard-virtualbox.box
+echo "${YELLOW}==> build.sh: list aviliable vagrant boxes ${YELLOW}\n"
+vagrant box list 
